16d772e9373cfe1b787a09fee9e6a0e7e34f73db
gerco <gerco.v.woudenbergh@gmail.com>
2022-04-03 22:31:55 +0200

    finished tutorial and start building own app

---
+++ flaskblog/models.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from flaskblog import db, login_manager
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Language(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    language = db.Column(db.String(100), nullable=False)
+    
+    def __repr__(self):
+        return f"Language('{self.language}')"
+    
+class Classes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    language_id = db.Column(db.Integer, db.ForeignKey('language.id'), nullable=False)
+    teacher_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    start = db.Column(db.DateTime, nullable=False)
+    location = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f"Language('{self.id}', '{self.language_id}', '{self.start}', '{self.location}')"
+
+
+
+# date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+# content = db.Column(db.Text, nullable=False)
+# user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
---
+++ readme.md
@@ -0,0 +1,3 @@
+```pip3 install flask wtforms flask_sqlalchemy flask-wtf email_validator flask-bcrypt flask-login Pillow```
+
+```python3 run.py```
