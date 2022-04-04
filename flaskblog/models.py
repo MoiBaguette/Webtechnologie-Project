43cad8b776692bf46ef4140772871d29e59ea233
Friedel Schön <derfriedmundschoen@gmail.com>
2022-04-04 14:49:15 +0200

    fill.py

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from flask_login import UserMixin
 
 from . import db, login_manager
---
+++ insert.py
@@ -0,0 +1,4 @@
+from flaskblog import bcrypt, db, models
+
+db.session.add(models.User(type='admin', username='admin', email='admin@email.com',
+               password=bcrypt.generate_password_hash('password').decode('utf-8')))
